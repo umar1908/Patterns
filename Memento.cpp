2f51ec8469ece23239dbcf470d9b9f76db3ed77c
umar1908 <umar190801@gmail.com>
2021-12-01 05:43:44 +0300

    Changing structure Clearing code Adding comments Setting up output

--- Memento.cpp
+++ Memento.cpp
@@ -2,85 +2,151 @@
 #include<stack>
 #include<string>
 
-//ПРОВЕРИТЬ CONST и OVERRIDE И ВИРТУАЛЬНЫЙ КОСТУРУКТОР
-//РЕАЛИЗОВАТЬ ХРАНИЛИЩЕ ДАМПОВ СО СТЕКОМ
 
+
+class TextEditor;
+
+//Абстрактный класс хранилища
 class AbstractMemento{
-protected:
+private:
   std::string name;
   std::string state;
   int hex;
+  TextEditor* original;
+
 public:
-  virtual std::string GetName() const=0;
-  virtual std::string GetSate() const=0;
-  virtual int GetHex() const=0;
+
+  AbstractMemento(std::string _state, int _hex, std::string _name,TextEditor* _original):name(_name),state(_state),hex(_hex),original(_original){}
+
+  virtual std::string GetName() const{
+    return name;
+  };
+
+  virtual std::string GetState() const{
+    return state;
+  };
+
+  virtual int GetHex() const{
+    return hex;
+  };
+
+  virtual TextEditor* GetOriginal()const{
+    return original;
+  }
+
+  virtual void recover() const=0;
+
   virtual ~AbstractMemento()=default;
 
 };
 
-
+//Класс под контролем паттерна снимок
 class TextEditor{
 private:
   std::string RealState;
   int RealHex;
-
+  // Конкретизация встроенного абстрактного класса хранилища
   class TextMemento:public AbstractMemento{
-  public:
-    TextMemento(const std::string& str, const int& _hex,std::string name){
-      state=str;
-      hex=_hex;
-    }
-    virtual std::string GetName() const override{
-      return name;
-    }
-    virtual std::string GetSate() const override{
-      return state;
-    }
-    virtual int GetHex() const override{
-      return hex;
-    }
-    virtual ~TextMemento()=default;
-  };
+
+      friend TextEditor; // Как ни парадоксально - обеспечивает инкапсуляцию, а не наоборот
+
+    private:
+
+        TextMemento(const std::string& str, const int& _hex,std::string name, TextEditor* original):AbstractMemento(str,_hex,name,original){}
+
+    public:
+
+      TextMemento()=delete;
+      virtual void recover() const override{
+        GetOriginal()->RealState=GetState();
+        GetOriginal()->RealHex=GetHex();
+      }
+
+      virtual ~TextMemento()=default;
+
+      };
 
 public:
 
   void EditText(const std::string& text){
     RealState=text;
   }
+
   void EditHex(const int& hex ){
     RealHex=hex;
   }
 
   void Status(){
-    std::cout<<"Current status:\nText:"<<RealState<<"\nHex:"<<RealHex<<std::endl;
+    std::cout<<"Current status:\nText:"<<RealState<<"\nHex:"<<RealHex<<"\n\n";
   }
 
-  TextMemento CreateMemento(std::string name="Unnamed") const{
-    return TextMemento(RealState, RealHex, name);
+  TextMemento* CreateMemento(std::string name="Unnamed") {
+    return new TextMemento(RealState, RealHex, name,this);
   }
 
-  void Recover(TextMemento mem){
-    RealState=mem.GetSate();
-    RealHex=mem.GetHex();
+};
+
+//Класс Опекуна
+class Caretaker{
+private:
+  std::stack<AbstractMemento*> history;
+  TextEditor* original;
+public:
+
+  Caretaker(TextEditor* _original):original(_original){}
+
+  void Save(std::string name="Unnamed") {
+    history.push(original->CreateMemento(name));
   }
 
+  void Undo(){
+    if(history.size()==0) throw std::range_error("History is empty");
+    history.top()->recover();
+    history.pop();
+  }
+
+  ~Caretaker(){
+    for (size_t i = 0; i < history.size(); i++) {
+      delete history.top();
+      history.pop();
+    }
+  }
 
 };
 
 
 int main(){
   TextEditor t1;
-  t1.EditText("text version 1");
-  t1.EditHex(123);
+  Caretaker backup(&t1);
+
+  //Последовательное изменение данных и сохранение изменений опекуном
+  t1.EditText("text1");
+  t1.EditHex(1);
+  t1.Status();
+  backup.Save();
+
+  t1.EditText("text2");
+  t1.EditHex(2);
+  t1.Status();
+  backup.Save();
+
+  t1.EditText("text3");
+  t1.EditHex(3);
+  t1.Status();
+  backup.Save();
+
+  //Последовательная отмена изменений
+  backup.Undo();
+  t1.Status();
+  backup.Undo();
+  t1.Status();
+  backup.Undo();
   t1.Status();
 
-  auto mem=t1.CreateMemento("version 1");
+  // backup.Undo();
+  // t1.Status();
 
-  t1.EditText("text version 2");
-  t1.EditHex(0);
-  t1.Status();
 
-  t1.Recover(mem);
-  t1.Status();
+
 
 }
