0a945185e42f4f53716a32b6cb221018b9c4ab6b
umar1908 <umar190801@gmail.com>
2021-12-01 05:58:45 +0300

    Clearing code adding comments setting up output

--- Visitor.cpp
+++ Visitor.cpp
@@ -3,36 +3,43 @@
 #include<list>
 
 class FacebookApi;
-// class InstagramApi;
 class VkApi;
-// class TwitterApi;
 
+//Абстрактный посетитель
 class AbstractVisitor{
 public:
-  virtual void visit(FacebookApi*) const=0;
-  virtual void visit(VkApi*) const =0;
 
+  virtual void visit(FacebookApi*) const=0;
+
+  virtual void visit(VkApi*) const =0;
 
 };
 
-
+//Абстрактный класс, функционал потомков которого будет расширяться
 class AbstractApi{
 protected:
   std::string name;
 public:
+
   virtual ~AbstractApi()=default;
+
   virtual void Accept(AbstractVisitor* )=0;
+
   virtual std::string GetName() const=0;
+
   AbstractApi(std::string _name):name(_name){}
 
 };
 
 
-
+//Первая конкретизация абстрактного класса
 class FacebookApi:public AbstractApi{
 public:
+
   virtual ~FacebookApi()=default;
+
   FacebookApi():AbstractApi("Facebook"){}
+
   void FacebookSendRequest(std::string request){
     std::cout<<"Request ("<<request<<") has been executed by FacebookApi\n";
   }
@@ -47,28 +54,34 @@ public:
 
 };
 
-  class VkApi:public AbstractApi{
-  public:
-    virtual ~VkApi()=default;
-    VkApi():AbstractApi("Vkontakte"){}
-    void VkSendRequest(std::string request){
-      std::cout<<"Request ("<<request<<") has been executed by VkApi\n";
-    }
+//Вторая конкретищация абстрактного класса
+class VkApi:public AbstractApi{
+public:
 
-    virtual void Accept(AbstractVisitor* visitor) override{
-      visitor->visit(this);
-    }
+  virtual ~VkApi()=default;
 
-    virtual std::string GetName() const override {
-      return name;
-    }
+  VkApi():AbstractApi("Vkontakte"){}
+
+  void VkSendRequest(std::string request){
+    std::cout<<"Request ("<<request<<") has been executed by VkApi\n";
+  }
+
+  virtual void Accept(AbstractVisitor* visitor) override{
+    visitor->visit(this);
+  }
+
+  virtual std::string GetName() const override {
+    return name;
+  }
 
 };
 
+// Первая конкретизация посетителя
 class ChangePassword:public AbstractVisitor{
 public:
   virtual ~ChangePassword()=default;
 private:
+
   virtual void visit(FacebookApi* api) const override {
     api->FacebookSendRequest("Change Facebook Password ******");
   }
@@ -80,9 +93,10 @@ private:
 
 };
 
-
-
+// Вторая конкретизация посетителя
 class SendMessage:public AbstractVisitor{
+public:
+
   virtual void visit(FacebookApi* api) const override {
     api->FacebookSendRequest("Send message by Facebook");
   }
@@ -98,12 +112,15 @@ class SendMessage:public AbstractVisitor{
 
 
 int main(){
+  //Создание основных объектов
   FacebookApi facebook;
   VkApi vk;
 
+  //Создание посетителей
   ChangePassword PasswordChanger;
   SendMessage MessageSender;
 
+  // Применение посетителей к основным объектам
   facebook.Accept(&PasswordChanger);
   vk.Accept(&PasswordChanger);
 
